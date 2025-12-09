3b1b95ae727f7605bd0baed9f1c02f9a58865895
Volodymyr Denisiuk <v@denisiuk.me>
2025-12-09 15:07:04 +0100

    fix: update canonical and Open Graph URLs in index.html; refactor path resolution in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,14 +16,17 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
         allowedHosts: ['webserver.imposanta.com']
       },
-      plugins: [basicSsl(),react()],
+      plugins: [
+        basicSsl(),
+        react()
+      ],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
         alias: {
-          '@': path.resolve(__dirname, '.'),
+          '@': resolve(__dirname, '.'),
         }
       }
     };
